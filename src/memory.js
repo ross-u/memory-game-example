291c451cbc05092829a1d722b5baf8e2067867fc
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2024-04-13 14:57:29 +0200

    initial commit

---
+++ src/memory.js
@@ -0,0 +1,66 @@
+class MemoryGame {
+  constructor(someCards) {
+    this.cards = someCards;
+    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsGuessed = 0;
+    this.pairsClicked = 0;
+    this.shuffleCards();
+  }
+
+  shuffleCards() {
+    if (!this.cards) {
+      return undefined;
+    };
+
+    let len = this.cards.length;
+    while (len > 0) {
+      len--;
+      let temp = this.cards[len];
+      let rdmInd = Math.floor(Math.random() * len);
+      this.cards[len] = this.cards[rdmInd];
+      this.cards[rdmInd] = temp;
+    }
+
+    return this.cards;
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+  }
+
+  checkIfFinished() {
+
+    const totalPairs = this.cards.length / 2;
+    if (this.pairsGuessed === totalPairs) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+// class MemoryGame {
+//   constructor(cards) {
+//     this.cards = cards;
+//     // add the rest of the class properties here
+//   }
+
+//   shuffleCards() {
+//     // ... write your code here
+//   }
+
+//   checkIfPair(card1, card2) {
+//     // ... write your code here
+//   }
+
+//   checkIfFinished() {
+//     // ... write your code here
+//   }
+// }
+
